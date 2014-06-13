2441bacbcb3916d9d75a3055b2a805ab1e471b73
Jonathon Fowler <fowlerj@usq.edu.au>
2014-06-13 10:58:55 +1000

    MDL-39913 assignsubmission_file: add file types restriction
    Testing for uploading a non-permitted file won't work as things stand
    currently because the filemanager throws an exception that Behat
    catches, which causes a fail.

--- mod/assign/submission/file/lang/en/assignsubmission_file.php
+++ mod/assign/submission/file/lang/en/assignsubmission_file.php
@@ -23,6 +23,9 @@
  */
 
 
+$string['acceptedfiletypes'] = 'Accepted file types';
+$string['acceptedfiletypes_help'] = 'Accepted file types can be restricted by entering a semicolon-separated list of mimetypes, for example \'video/mp4, audio/mp3, image/png, image/jpg\'. If the field is left empty, then all file types are allowed.';
+$string['siteuploadlimit'] = 'Site upload limit';
 $string['configmaxbytes'] = 'Maximum file size';
 $string['countfiles'] = '{$a} files';
 $string['default'] = 'Enabled by default';
@@ -31,6 +34,8 @@ $string['enabled'] = 'File submissions';
 $string['enabled_help'] = 'If enabled, students are able to upload one or more files as their submission.';
 $string['eventassessableuploaded'] = 'A file has been uploaded.';
 $string['file'] = 'File submissions';
+$string['filesofthesetypes'] = 'Files of these types may be added to the submission:';
+$string['filetypewithexts'] = '{$a->name} &mdash; {$a->extlist}';
 $string['maxbytes'] = 'Maximum file size';
 $string['maxfiles'] = 'Maximum files per submission';
 $string['maxfiles_help'] = 'If file submissions are enabled, each assignment can be set to accept up to this number of files for their submission.';
@@ -40,5 +45,7 @@ $string['maximumsubmissionsize'] = 'Maximum submission size';
 $string['maximumsubmissionsize_help'] = 'Files uploaded by students may be up to this size.';
 $string['numfilesforlog'] = 'The number of file(s) : {$a} file(s).';
 $string['pluginname'] = 'File submissions';
+$string['setting_filetypes'] = 'Permitted file types';
+$string['setting_filetypes_desc'] = 'File types and groups chosen here become the options selectable for configuration of the submission method. Allowed file types can be restricted by entering a semicolon-separated list of mimetypes, for example \'video/mp4, audio/mp3, image/png, image/jpg\'. If the field is left empty, then all file types are allowed.';
 $string['siteuploadlimit'] = 'Site upload limit';
 $string['submissionfilearea'] = 'Uploaded submission files';
