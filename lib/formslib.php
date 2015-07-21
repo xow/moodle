f3ddbc653611ff3b470ef733768f76e22dd5ac03
John Okely <john@moodle.com>
2015-07-21 08:01:49 +0000

    MDL-45772 admin: Stop browsers from autofilling passwords incorrectly

--- lib/formslib.php
+++ lib/formslib.php
@@ -188,6 +188,8 @@ abstract class moodleform {
             $this->_form->hardFreeze();
         }
 
+        $this->_form->addElement('hiddenpassword');
+
         $this->definition();
 
         $this->_form->addElement('hidden', 'sesskey', null); // automatic sesskey protection
@@ -2939,6 +2941,7 @@ MoodleQuickForm::registerElementType('grading', "$CFG->libdir/form/grading.php",
 MoodleQuickForm::registerElementType('group', "$CFG->libdir/form/group.php", 'MoodleQuickForm_group');
 MoodleQuickForm::registerElementType('header', "$CFG->libdir/form/header.php", 'MoodleQuickForm_header');
 MoodleQuickForm::registerElementType('hidden', "$CFG->libdir/form/hidden.php", 'MoodleQuickForm_hidden');
+MoodleQuickForm::registerElementType('hiddenpassword', "$CFG->libdir/form/hiddenpassword.php", 'MoodleQuickForm_hiddenpassword');
 MoodleQuickForm::registerElementType('htmleditor', "$CFG->libdir/form/htmleditor.php", 'MoodleQuickForm_htmleditor');
 MoodleQuickForm::registerElementType('listing', "$CFG->libdir/form/listing.php", 'MoodleQuickForm_listing');
 MoodleQuickForm::registerElementType('modgrade', "$CFG->libdir/form/modgrade.php", 'MoodleQuickForm_modgrade');
