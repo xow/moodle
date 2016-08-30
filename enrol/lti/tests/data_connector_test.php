7132b3697f97b612b6e2aa38a6aed3a9f0a97638
John Okely <john@moodle.com>
2016-08-30 11:24:14 +0800

    MDL-53832 enrol_lti: Split launch and proxy registration end points
    Original subtask: MDL-55752.

--- enrol/lti/tests/data_connector_test.php
+++ enrol/lti/tests/data_connector_test.php
@@ -400,7 +400,7 @@ class enrol_lti_data_connector_testcase extends advanced_testcase {
     }
 
     /**
-     * Test for data_connector::loadContext().
+     * Test for data_connector::saveContext().
      */
     public function test_save_context() {
         $dc = new data_connector();
